cb75562d793210fd064bc77c56767345830f3d43
Ivan Faathirza <49663880+ivan0811@users.noreply.github.com>
2020-10-28 12:00:17 +0700

    initial commit

--- src/com/perbandingan/Main.java
+++ src/com/perbandingan/Main.java
@@ -48,6 +48,7 @@ public class Main {
 
     public static void main(String[] args) {
 	    Main data = new Main();
-        data.startMethod();
+            data.startMethod();
+	    System.out.println("Developed by : Ivan Faathirza");
     }
 }
